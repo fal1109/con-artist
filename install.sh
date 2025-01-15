b5553c2d962238268d921089d516523eff103516
fal <160747481+fal1109@users.noreply.github.com>
2025-01-15 18:15:14 +0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,66 @@
+# install yay
+
+sudo pacman -S yay
+
+#install git
+
+yay -S git
+
+# hyprland setup 
+
+yay -S hyprland hyprlock hypridle hyprutils hyprgraphics hyprcursor hyprwayland-scanner hyprshot rofi tofi 
+
+# dotfiles dependencies and terminal flex things
+
+yay -S --noconfirm waybar-cava cava cmus networkmanager
+
+yay -S --noconfirm btop fastfetch kitty neovim
+
+yay -s --noconfirm network-manager-applet thunar swww pywal
+
+yay -S --noconfirm blueman pavucontrol nwg-look nwg-desktops mpv sxiv
+
+yay -S --noconfirm cbonsai yazi alsamixer easyeffects
+
+# hyprland dotfiles 
+
+git clone https://github.com/fal1109/con_artist.git
+
+cp -r dotfiles/.config ~/
+cp dotfiles/.zshrc ~/
+cp dotfiles/.bashrc ~/
+
+# sddm dotfiles
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)"
+
+# apps
+
+yay -S --noconfirm vesktop floorp-bin spotify nautilus
+
+
+yay -S --noconfirm code qbittorrent obs-studio
+
+yay -S --noconfirm  obsidian miru-bin proton-vpn-gtk-app proton-mail
+
+# games 
+
+yay -S --noconfirm tetrio-plus-bin osu-lazer pokerogue
+
+# emulators
+
+yay -S --noconfirm wine 
+
+yay -S --noconfirm scrcppy
+
+yay -S --noconfirm waydroid
+
+# set shell to fish
+
+yay -S --noconfirm fish
+
+chsh -s /usr/bin/fish
+
+# reboot
+
+sudo reboot
